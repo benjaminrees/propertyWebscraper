1354df507465bc4efa77242f42231141f0b478c5
Benjamin Rees <brees@scottlogic.com>
2022-05-13 14:53:50 +0100

    change filestructure

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { westernLettingsScrape } = require("./westernLettings");
 const PORT = 8000;
 
 const axios = require("axios");
@@ -14,25 +15,5 @@ const westernLettingsURL =
 app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
 
 console.log("Western Lettings Details");
-axios(westernLettingsURL)
-    .then((response) => {
-        const html = response.data;
-        let prices = [];
 
-        let $ = cheerio.load(html);
-
-        $(".property_price").each((i, el) => {
-            prices.push($(el).text());
-        });
-
-        console.log(prices);
-
-        // # searches for ID
-        let numbers = $("#wl_search_count");
-        numberText = numbers.text();
-
-        console.log(numberText);
-        // regex expression that extracts both of the numbers
-        console.log(numberText.match(/\d+/g));
-    })
-    .catch((error) => console.log(error));
+westernLettingsScrape();
